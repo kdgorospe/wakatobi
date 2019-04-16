6ed6b96bcea8f8450884b2a4ff162f1e80c7f659
KGthatsme <kdgorospe@gmail.com>
2019-04-16 13:23:32 -0400

    Melati questions

--- cleanWakatobi.R
+++ cleanWakatobi.R
@@ -2,15 +2,15 @@
 # Contact: Austin Humphries, Paul Carvalho, Kelvin Gorospe, Lauren Josephs
 
 
-### STEP ONE: setwd()
-setwd("~/Analyses")
+### STEP ONE: set your preferred working directory using setwd() in console
+
 
 rm(list=ls())
 library(googledrive)
 
 
 # Download raw data files from SHERA google drive folder
-# Will require you to sign into Google account and grant permission to tidyverse for access 
+drive_auth() # Will require you to sign into Google account and grant permission to tidyverse for access 
 drive_download("Wakatobi-fish-data_041119.csv", overwrite=TRUE)  # saves file locally; overwrite in case you've downloaded it before and want the most up-to-date
 landings<-read.csv("Wakatobi-fish-data_041119.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
 
@@ -25,3 +25,8 @@ dim(trips)
 mergecol<-names(landings)[names(landings) %in% names(trips)]
 
 tempdat<-merge(landings, trips, by=mergecol)
+
+# Questions for Melati?
+# table(trips$trip_id) # why are some trip_IDs not unique? 
+tempdat[tempdat$trip_id=="1 11 ACER + DULETES",]
+tempdat[tempdat$Fish_name_p=="Kumai",]
