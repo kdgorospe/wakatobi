e84d10445185496057c8840fd4fed03600a3a357
KGthatsme <kdgorospe@gmail.com>
2019-04-17 11:12:55 -0400

    new filenames

--- cleanWakatobi.R
+++ cleanWakatobi.R
@@ -3,7 +3,7 @@
 
 
 ### STEP ONE: set your preferred working directory using setwd() in console
-
+### setwd("~/Analyses")
 
 rm(list=ls())
 library(googledrive)
@@ -11,12 +11,12 @@ library(googledrive)
 
 # Download raw data files from SHERA google drive folder
 drive_auth() # Will require you to sign into Google account and grant permission to tidyverse for access 
-drive_download("Wakatobi-fish-data_041119.csv", overwrite=TRUE)  # saves file locally; overwrite in case you've downloaded it before and want the most up-to-date
-landings<-read.csv("Wakatobi-fish-data_041119.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
+drive_download("Wakatobi-landings_041119_FISH.csv", type="csv", overwrite=TRUE)  # saves file locally; overwrite in case you've downloaded it before and want the most up-to-date
+landings<-read.csv("Wakatobi-landings_041119_FISH.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
 
 
-drive_download("Wakatobi-trip-data_041119.csv", overwrite=TRUE)
-trips<-read.csv("Wakatobi-trip-data_041119.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
+drive_download("Wakatobi-landings_041119_TRIP.csv", overwrite=TRUE)
+trips<-read.csv("Wakatobi-landings_041119_TRIP.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
 
 
 dim(landings)
@@ -26,8 +26,6 @@ mergecol<-names(landings)[names(landings) %in% names(trips)]
 
 tempdat<-merge(landings, trips, by=mergecol)
 
-# Questions for Melati?
 # table(trips$trip_id) # why are some trip_IDs not unique? 
 tempdat[tempdat$trip_id=="1 11 ACER + DULETES",]
-tempdat[tempdat$Fish_name_p=="Kumai",]
-# TESTING HOW QUICK IT PUSHES
+
