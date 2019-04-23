78ad3c71e9ae21286cfa872de0b2510e25feb408
KGthatsme <kdgorospe@gmail.com>
2019-04-23 10:24:31 -0400

    use file id for download function

--- cleanWakatobi.R
+++ cleanWakatobi.R
@@ -11,12 +11,18 @@ library(googledrive)
 
 # Download raw data files from SHERA google drive folder
 drive_auth() # Will require you to sign into Google account and grant permission to tidyverse for access 
-drive_download("Wakatobi-landings_041119_FISH.csv", type="csv", overwrite=TRUE)  # saves file locally; overwrite in case you've downloaded it before and want the most up-to-date
+#drive_download("Wakatobi-landings_041119_FISH.csv", type="csv", overwrite=TRUE)  # saves file locally; overwrite in case you've downloaded it before and want the most up-to-date
+#file id: 1_71pviN-qsSmvAZAgQMCQIqkEXpKTPcd
+#Use File ID method: more specific file identification
+drive_download(as_id("1_71pviN-qsSmvAZAgQMCQIqkEXpKTPcd"), overwrite=TRUE) # Saves file to working directory
 landings<-read.csv("Wakatobi-landings_041119_FISH.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
 
 
-drive_download("Wakatobi-landings_041119_TRIP.csv", overwrite=TRUE)
+#drive_download("Wakatobi-landings_041119_TRIP.csv", overwrite=TRUE)
+#file id: 1lVYpdmf8i0BZYbK5iQxXn2eFR1klxWbC
+drive_download(as_id("1lVYpdmf8i0BZYbK5iQxXn2eFR1klxWbC"), overwrite=TRUE) 
 trips<-read.csv("Wakatobi-landings_041119_TRIP.csv", header=T, stringsAsFactors = FALSE, strip.white = TRUE)
+
 
 
 dim(landings)
@@ -35,17 +41,5 @@ tempdat$Trip_time_unit[grep("day", tempdat$Trip_time_unit)]<-"d"
 tempdat$Trip_time_unit[grep("hour", tempdat$Trip_time_unit)]<-"h"
 tempdat$Trip_time_hours<-tempdat$Trip_time_no
 tempdat<-transform(tempdat, Trip_time_hours=ifelse(Trip_time_unit=="d", Trip_time_hours*24, Trip_time_hours))
-# PRACTICE EDITING DIFFERENT SECTIONS OF THE CODE
 
-
-## Kelly is my favorite lab mascot - Lauren 
-
-# Paul's doing all of this data cleaning for us
-
-# paul's comment
-kelvin = "kelly's dad"
-
-
-kelvin<-strsplit(kelvin, " ")
-kelvin[[1]][grep("dad", kelvin)]<-"cool dude"
 
